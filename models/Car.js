5115ef9aa9010b8534954865086982e4896b9482
ARod <ajr.rodriguez095@gmail.com>
2022-04-06 19:36:33 -0400

    added car model

---
+++ models/Car.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const { model } = require('../config/connection');
+const sequelize = require('../config/connection');
+
+//User model configuration
+class Car extends Model {}
+
+Car.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement:true
+        },
+        model: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        trim: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        retailPrice: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'car'
+    }
+);
+
+module.exports = User;
