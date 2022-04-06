a28d4992a02583218fa2acc60be8a1e396fea5f9
ARod <ajr.rodriguez095@gmail.com>
2022-04-06 17:39:50 -0400

    trying to set up for user creation and authentication for login

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const { model } = require('../config/connection');
+const sequelize = require('../config/connection');
+
+//User model configuration
+class User extends Model {}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement:true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+model.exports = User;
