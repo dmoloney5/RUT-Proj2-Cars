4b693e26ba00c029f8d3b9b0776c02f150ac2ac0
ARod <ajr.rodriguez095@gmail.com>
2022-04-06 21:10:43 -0400

    added delete route. working on create

--- controllers/api/car-routes.js
+++ controllers/api/car-routes.js
@@ -53,8 +53,21 @@ router.put('/', (req, res) => {
 // Delete specified car in database
 router.delete('/', (req, res) => {
     Car.destroy({
-
+        where: {
+            id: req.params.id
+        }
     })
-})
+    .then(dbCarData => {
+        if(!dbCarData) {
+            res.status(404).json({ message: 'No car found with this id'});
+            return;
+        }
+        res.json(dbCarData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
