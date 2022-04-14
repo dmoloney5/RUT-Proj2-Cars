613e945759775ca85f88957b8f41f1f92ac87ee8
techman <dmoloney5@gmail.com>
2022-04-13 20:39:12 -0400

    got homepage to work

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const sequelize = require("../config/connection");
 //const withAuth = require("../utils/auth");
 
+
 router.get('/login', (req, res) => {
     res.render("login")
 })
@@ -14,16 +15,16 @@ router.get('/car-search', (req, res) => {
     res.render("car-search")
 })
 
-router.get('/dashboard', (req, res) => {
-    res.render("Dashboard")
+router.get('/', (req, res) => {
+    res.redirect("/homepage")
 })
 
 router.get('/favorites', (req, res) => {
     res.render("favorites")
 })
 
-router.get('/homepage', (req, res) => {
-    res.render("homepage")
+router.get('/dashboard', (req, res) => {
+    res.render("dashboard")
 })
 
 router.get('/new-post', (req, res) => {
