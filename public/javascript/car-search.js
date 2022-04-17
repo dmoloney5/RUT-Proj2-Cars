b8dd85e04abbba0eb17d1285a624250242ce390e
Hannah Bush <heb.rutgers@gmail.com>
2022-04-17 14:40:00 -0400

    more tweaks

--- public/javascript/car-search.js
+++ public/javascript/car-search.js
@@ -6,15 +6,14 @@ document.getElementById("search-submit").addEventListener("click", () => {
   var price = document.getElementById("price").value;
   //var body= document.getElementById("body").value;
 
-  const response = await fetch(`/api/cars/${make}/${mileage}/${price}/${body}`, {
+  const response = await fetch(`/api/cars/${make}/${mileage}/${price}`, {
     method: "GET",
 
     headers: { "Content-Type": "application/json" },
   });
   if (response.ok) {
     console.log("success");
-
-    document.location.replace("/dashboard");
+    alert("Your search was successful!");
   } else {
     alert(response.statusText);
   }
