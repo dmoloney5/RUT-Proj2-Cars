6d71f6695642250751d125fa58805debe5f75af9
Angelo <angelocsaverino@gmail.com>
2022-04-17 16:25:13 -0400

    no more crashes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,8 @@
-const { User } = require("../models");
+const { User, Car } = require("../models");
 const router = require("express").Router();
 const sequelize = require("../config/connection");
 const path = require("path");
-//const withAuth = require("../utils/auth");
+const withAuth = require("../utils/auth");
 
 router.get("/login", (req, res) => {
   res.render("login");
@@ -40,22 +40,28 @@ router.get('/dashboard', (req, res) => {
   });
 })
 
-router.get("/new-post", async (req, res) => {
-  const car = await Car.findByPk(req.session.user_id)
-  res.render("new-post", car.toJSON());
+router.get("/new-post", withAuth, async (req, res) => {
+  // const car = await Car.findByPk(req.session.user_id)
+  // res.render("new-post", car.toJSON());
+  res.render("new-post",  {
+    loggedIn: req.session.loggedIn
+  });
 });
 
-router.get("/settings", async (req, res) => {
-  const user = await User.findByPk(req.session.user_id)
-  res.render("settings", user.toJSON(), {
+router.get("/settings", withAuth, async (req, res) => {
+  // const user = await User.findByPk(req.session.user_id)
+  res.render("settings", {
     loggedIn: req.session.loggedIn,
     user: req.session.username
   });
 });
 
-router.get("/your-profile", async (req, res) => {
-  const user = await User.findByPk(req.session.user_id)
-  res.render("your-profile", user.toJSON());
+router.get("/your-profile", withAuth, async (req, res) => {
+  // const user = await User.findByPk(req.session.user_id)
+  // res.render("your-profile", user.toJSON());
+  res.render("your-profile",  {
+    loggedIn: req.session.loggedIn
+  });
 });
 
 module.exports = router;
