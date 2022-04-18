d2df117de5d747245a798d70128ab544d25abf8b
Hannah Bush <heb.rutgers@gmail.com>
2022-04-18 13:36:53 -0400

    require car info to post

--- public/javascript/new-post.js
+++ public/javascript/new-post.js
@@ -6,33 +6,35 @@ document.getElementById("post-submit").addEventListener("click", () => {
   const Make = document.getElementById("make").value;
   const Model = document.getElementById("model").value;
   const Series = document.getElementById("series").value;
-  const Color= document.getElementById("color").value;
+  const Color = document.getElementById("color").value;
   const Mileage = document.getElementById("mileage").value;
   const Price = document.getElementById("price").value;
   const Description = document.getElementById("description").value;
 
-  const response = await fetch(`/api/cars`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      Year,
-      Make,
-      Model,
-      Series,
-      Color,
-      Mileage,
-      Price,
-      Description
-    }),
-  });
-  if (response.ok) {
-    console.log("success");
-    alert("Your car has successfully been posted!");
-    document.location.replace("/dashboard");
-  } else {
-    alert(response.statusText);
+  if (Year && Make && Model && Series && Color && Mileage && Price) {
+    const response = await fetch(`/api/cars`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        Year,
+        Make,
+        Model,
+        Series,
+        Color,
+        Mileage,
+        Price,
+        Description
+      }),
+    });
+    if (response.ok) {
+      console.log("success");
+      alert("Your car has successfully been posted!");
+      //document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
+    }
   }
 
 })
