7ac57847040d7d6e39c16f8c532a7fff718176a2
techman <dmoloney5@gmail.com>
2022-04-17 20:40:01 -0400

    fixing settings

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -40,16 +40,17 @@ router.get("/new-post", withAuth, async (req, res) => {
 });
 
 router.get("/settings", withAuth, async (req, res) => {
-  // const user = await User.findByPk(req.session.user_id)
-  // res.render("settings", {
+  const user = await User.findByPk(req.session.user_id)
+  res.render("settings", {
     loggedIn: req.session.loggedIn,
     user: user.toJSON()
   });
 });
 
 router.get("/settings", withAuth, async (req, res) => {
-  //  const user = await User.findByPk(req.session.user_id)
-  // res.render("settings", user.toJSON());
+ 
+  const user = await User.findByPk(req.session.user_id)
+  res.render("settings", user.toJSON());
   res.render("settings", {
     loggedIn: req.session.loggedIn,
     user: user.toJSON()
@@ -58,8 +59,8 @@ router.get("/settings", withAuth, async (req, res) => {
 
 
 router.get("/your-profile", withAuth, async (req, res) => {
-  // const user = await User.findByPk(req.session.user_id)
-  // res.render("your-profile", user.toJSON());
+  const user = await User.findByPk(req.session.user_id)
+  res.render("your-profile", user.toJSON());
   res.render("your-profile",  {
     loggedIn: req.session.loggedIn
   });
