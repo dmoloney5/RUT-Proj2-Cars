47ae7fcfe92f901495ceabbcfd02dbe187215f7b
Hannah Bush <heb.rutgers@gmail.com>
2022-04-18 13:12:39 -0400

    drop favoritelist and post models; not used

--- models/FavoriteList.js
+++ models/FavoriteList.js
@@ -1,42 +1,42 @@
-const { Model, DataTypes } = require('sequelize');
+// const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('../config/connection');
+// const sequelize = require('../config/connection');
 
-class FavoriteList extends Model {}
+// class FavoriteList extends Model {}
 
-FavoriteList.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      reference: {
-        model: "user",
-        key: "id",
-      },
-    },
-    car_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "car",
-        key: "id",
-      },
-      car_img: {
-        type: DataTypes.CHAR,
-      }
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "favorite_list",
-  }
-);
+// FavoriteList.init(
+//   {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: true,
+//     },
+//     user_id: {
+//       type: DataTypes.INTEGER,
+//       reference: {
+//         model: "user",
+//         key: "id",
+//       },
+//     },
+//     car_id: {
+//       type: DataTypes.INTEGER,
+//       references: {
+//         model: "car",
+//         key: "id",
+//       },
+//       car_img: {
+//         type: DataTypes.CHAR,
+//       }
+//     },
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: "favorite_list",
+//   }
+// );
 
-module.exports = FavoriteList;
+// module.exports = FavoriteList;
--- models/Post.js
+++ models/Post.js
@@ -1,42 +1,42 @@
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connection");
+// const { Model, DataTypes } = require("sequelize");
+// const sequelize = require("../config/connection");
 
-class Post extends Model {}
+// class Post extends Model {}
 
-Post.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    cars: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    retail_price: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "user",
-        key: "id",
-      },
-      car_img: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-    },
-  },
-  {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "post",
-  }
-);
+// Post.init(
+//   {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: true,
+//     },
+//     cars: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     retail_price: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     user_id: {
+//       type: DataTypes.INTEGER,
+//       references: {
+//         model: "user",
+//         key: "id",
+//       },
+//       car_img: {
+//         type: DataTypes.STRING,
+//         allowNull: false
+//       },
+//     },
+//   },
+//   {
+//     sequelize,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: "post",
+//   }
+// );
 
-module.exports = Post;
+// module.exports = Post;
